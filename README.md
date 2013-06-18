627fba8ecc2c92809bc6aa71d6374861ef50106c
Leeroy Brun <leeroy.brun@gmail.com>
2013-06-18 14:47:38 +0200

    Initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-node-express-https
-==================
+# node-express-https
 
 Example of using Express &amp; Node.js with HTTPS.
+
+## Generation of SSL keys
+
+Create the private key :
+
+```shell
+openssl genrsa -out key.pem 1024
+```
+
+Create the "Certificate Signing Request" :
+
+```shell
+openssl req -new -key key.pem -out csr.pem
+```
+
+Create the self-signed certificate :
+```shell
+openssl x509 -req -in csr.pem -signkey key.pem -out cert.pem
+```
---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require('express'),
+    https = require('https'),
+    fs = require('fs');
+
+var PORT = process.env.PORT || 8080;
+var HOST = process.env.HOST || '';
+
+var app = express();
+
+app.configure(function() {
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.static('public'));
+    app.use(app.router);
+});
+
+app.configure('development', function(){
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+	app.use(express.errorHandler());
+});
+
+app.get('/', function(req, res) {
+    res.end('Welcome on the secure server !');
+});
+
+var options = {
+    key  : fs.readFileSync('ssl/key.pem'),
+    ca   : fs.readFileSync('ssl/csr.pem'),
+    cert : fs.readFileSync('ssl/cert.pem')
+}
+
+https.createServer(options, app).listen(PORT, HOST, null, function() {
+    console.log('Server listening on port %d in %s mode', this.address().port, app.settings.env);
+});
